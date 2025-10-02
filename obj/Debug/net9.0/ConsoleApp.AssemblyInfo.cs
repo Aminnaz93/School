cc724f1c74300d233a7535505fa69271a4a12b1a
aminnaz93 <Aminnazari040@hotmail.com>
2025-10-02 14:20:27 +0200

    new updates

--- obj/Debug/net9.0/ConsoleApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dab8281a2f01fb88b41b5339940863c847fdfb1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cf0bb5442e4d4ed0e0aff0493613530779d257a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
